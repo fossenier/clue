892609d6cf0003021185a9bac7bc5770d5a423c8
Logan Fossenier <logan.fossenier@gmail.com>
2024-09-26 18:03:57 -0600

    chore: add comments and analyze how to display grid properly

--- web2/clue-app/app/play/page.tsx
+++ web2/clue-app/app/play/page.tsx
@@ -13,6 +13,7 @@ export default function play() {
       <div
         className={`grid w-fit h-fit`}
         style={{
+          // Make each tile take up the same amount of vertical and horizontal space
           gridTemplateRows: `repeat(25, 1fr)`,
           gridTemplateColumns: `repeat(25, 1fr)`,
         }}
@@ -22,13 +23,15 @@ export default function play() {
             onClick={() => updateBoardData(idx)}
             key={idx}
             className={`square w-6 h-6 flex items-center justify-center border-2 border-gray-300 cursor-pointer text-sm font-bold text-white ${
+              // Set the tile's colour based on the contents of the tile
               cell === "x"
                 ? "bg-red-600"
                 : cell === " " || cell === "door"
-                ? "bg-yellow-200"
-                : "bg-black"
+                  ? "bg-yellow-200"
+                  : "bg-black"
             }`}
           >
+            {/* Display the contents of the cell (ex. room name) when it isn't a room or hallway */}
             {cell !== "x" && cell !== " " ? cell : ""}
           </div>
         ))}
