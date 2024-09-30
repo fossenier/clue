350e5640ce1a0d24e408d2d825bb27245d8b43fd
Logan Fossenier <logan.fossenier@gmail.com>
2024-09-30 10:43:58 -0600

    feat: factor the board out into a component

--- web2/clue-app/app/play/page.tsx
+++ web2/clue-app/app/play/page.tsx
@@ -1,34 +1,18 @@
+// app/play/page.tsx
+
 "use client";
 import React from "react";
 
-import { BOARD, ROOMS, SUSPECTS, WEAPONS } from "@constants/index";
+import Board from "./Board";
 
-export default function play() {
+export default function Play() {
   const updateBoardData = (idx: number) => {
     console.log(idx);
   };
 
   return (
-    <div className="h-dvh w-dvw">
-      {BOARD.map((row, rowIndex) => (
-        <div key={`row${rowIndex}`} className="h-1/24 flex">
-          {row.map((tile, tileIndex) => (
-            <div
-              key={rowIndex * 24 + tileIndex}
-              className={`w-1/25 border-2 border-gray-300 box-border cursor-pointer text-sm font-bold text-white ${
-                // Set the tile's colour based on the contents of the tile
-                tile === "x"
-                  ? "bg-red-600"
-                  : tile === " " || tile === "door"
-                    ? "bg-yellow-200"
-                    : "bg-black"
-              }`}
-            >
-              {tile !== "x" && tile !== " " ? tile : ""}
-            </div>
-          ))}
-        </div>
-      ))}
+    <div>
+      <Board onTileClick={updateBoardData} />
     </div>
   );
 }
